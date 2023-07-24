12f0b9e6d3c837ee79ded798d58d77044d022697
Artem Bushmin <nim-shub@rambler.ru>
2023-07-24 22:28:10 +0300

    Тесты не проходят2

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -13,25 +13,20 @@ from .serializers import (
     GroupSerializer,
     FollowSerializer,
 )
+from .permissions import OwnerOrReadOnly
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     pagination_class = LimitOffsetPagination
+    permission_classes = (
+        permissions.IsAuthenticatedOrReadOnly,
+        OwnerOrReadOnly,
+    )
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
-
-    def perform_update(self, serializer):
-        if serializer.instance.author != self.request.user:
-            raise PermissionDenied("Изменение чужого контента запрещено!")
-        super(PostViewSet, self).perform_update(serializer)
-
-    def perform_destroy(self, instance):
-        if instance.author != self.request.user:
-            raise PermissionDenied("Удаление чужого контента запрещено!")
-        return super(PostViewSet, self).perform_destroy(instance)
 
 
 class CommentViewSet(viewsets.ModelViewSet):
